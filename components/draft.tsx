8f82bae1dc36f207cf6a664658dd2780f73dfc00
ShinnosukeUesaka <shinnosuke.uesaka@gmail.com>
2024-02-03 20:40:41 -0800

    Refactor page and draft components

--- components/draft.tsx
+++ components/draft.tsx
@@ -1,11 +1,16 @@
-import React from "react" 
-import dynamic from 'next/dynamic'
-
+import React from "react";
+import dynamic from "next/dynamic";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
 export function Draft() {
-    return (
-        <div className="flex flex-col w-full h-full">
-        <h1 className="text-2xl m-4">Drafting</h1>
-        </div>
-    )
+  return (
+    <div className="flex flex-col w-full h-full justify-between">
+      <div className="m-5 h-[50px] flex items-center text-2xl">Drafting</div>
+      <Textarea className="p-10 w-full text-lg flex-1" placeholder="Write here" />
+      <div className="h-[100px] flex justify-center items-center">
+        <Button className="">Memofy</Button>
+      </div>
+    </div>
+  );
 }
--- components/mainpage.tsx
+++ components/mainpage.tsx
@@ -16,11 +16,11 @@ import { Draft } from "./draft";
 
 export function Mainpage() {
   return (
-    <div className="min-h-screen w-full flex ">
+    <div className="h-full w-full flex">
       <div className="border-r w-[300px]">
         <ScrollAreaDemo />
       </div>
-      <div className="w-full">
+      <div className="w-full h-full">
         <Draft />
       </div>
     </div>
