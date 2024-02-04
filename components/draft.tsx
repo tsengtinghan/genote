c40aa91af56bf9794df1ed040f70b2bc5720b848
Michael Tseng <michael08120@gmail.com>
2024-02-03 22:32:23 -0800

    update draft padding

--- components/draft.tsx
+++ components/draft.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import dynamic from "next/dynamic";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-
+import { Separator } from "@radix-ui/react-separator";
 export function Draft() {
   return (
     <div className="flex flex-col w-full h-full justify-between">
       <div className="m-5 h-[50px] flex items-center text-2xl">Drafting</div>
-      <Textarea className="p-10 w-full text-lg flex-1" placeholder="Write here" />
+      <Textarea
+        className="pl-10 w-full text-lg flex-1"
+        placeholder="Write here"
+      />
       <div className="h-[100px] flex justify-center items-center">
         <Button className="">Memofy</Button>
       </div>
