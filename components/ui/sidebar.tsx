96a7fc9f73035bf7ba7745b2fa26a06fd932f9fb
Michael Tseng <michael08120@gmail.com>
2024-02-03 19:48:42 -0800

    delete trash code in scrollarea

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -30,22 +30,3 @@ export function ScrollAreaDemo() {
 }
 
 
-function StickyNoteIcon(props) {
-    return (
-      <svg
-        {...props}
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M15.5 3H5a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2V8.5L15.5 3Z" />
-        <path d="M15 3v6h6" />
-      </svg>
-    )
-  }
